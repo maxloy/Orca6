56b831398de96a2dfcdb9273b5537b4a0ca04abd
D. Max Loy <morac@morac.net>
2015-08-01 15:54:55 -0700

    Added DestroyInTime script

---
+++ Assets/UtilityScripts/DestroyInTime.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyInTime: MonoBehaviour
+{
+	public float Duration = 1f;
+
+	float EndTime = 0;
+
+	void OnEnable()
+	{
+		EndTime = Time.time + Duration;
+	}
+
+	void Update()
+	{
+		if(Time.time < EndTime)
+		{
+			gameObject.Recycle();
+		}
+	}
+}
