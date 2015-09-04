66202d2a982c447d6109403e89920ff5786a961d
D. Max Loy <morac@morac.net>
2015-09-03 19:56:01 -0700

    Static Utility class and Vector midpoint

---
+++ Assets/UtilityScripts/Utility.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class Utility
+{
+	public static Vector3 Midpoint(Vector3 a, Vector3 b)
+	{
+		return a + (a - b) / 2f;
+	}
+
+	public static Vector4 Midpoint(Vector4 a, Vector4 b)
+	{
+		return a + (a - b) / 2f;
+	}
+
+	public static Vector2 Midpoint(Vector2 a, Vector2 b)
+	{
+		return a + (a - b) / 2f;
+	}
+	
+}
