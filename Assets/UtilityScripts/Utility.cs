5860c946ac627bd3b929cab9b454850c044eca25
D. Max Loy <morac@morac.net>
2015-09-06 00:01:48 -0700

    I can totally math, what are you talking about?

--- Assets/UtilityScripts/Utility.cs
+++ Assets/UtilityScripts/Utility.cs
@@ -4,17 +4,17 @@ public static class Utility
 {
 	public static Vector3 Midpoint(Vector3 a, Vector3 b)
 	{
-		return a + (a - b) / 2f;
+		return (a + b) / 2f;
 	}
 
 	public static Vector4 Midpoint(Vector4 a, Vector4 b)
 	{
-		return a + (a - b) / 2f;
+		return (a + b) / 2f;
 	}
 
 	public static Vector2 Midpoint(Vector2 a, Vector2 b)
 	{
-		return a + (a - b) / 2f;
+		return (a + b) / 2f;
 	}
 	
 }
