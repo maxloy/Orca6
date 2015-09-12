d28fd488d5358849dbf96f85ec5631fc101e94da
D. Max Loy <morac@morac.net>
2015-09-12 16:10:07 -0700

    font, level tweaks

---
+++ Assets/Scripts/GoToNextLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GoToNextLevel : MonoBehaviour
+{
+
+
+	public void Update()
+	{
+		if(GameManager.CurrentState != GameManager.State.Playing && GameManager.CurrentLevelIndex < GameManager.Instance.Levels.Count - 1)
+		{
+			GameManager.Instance.GoToLevel(GameManager.CurrentLevelIndex + 1);
+			GameManager.Instance.GoToState(GameManager.State.Playing);
+		}
+	}
+}
---
+++ Assets/Scripts/TranslateWithInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TranslateWithInput : MonoBehaviour
+{
+
+	public float MoveSpeed = 1;
+
+	void Update()
+	{
+		transform.position += new Vector3
+			(
+				Input.GetAxis("Right_X_Axis") * MoveSpeed,
+				0,
+				Input.GetAxis("Right_Y_Axis") * MoveSpeed
+			);
+		transform.position += new Vector3
+			(
+				Input.GetAxis("Mouse X") * MoveSpeed,
+				0,
+				Input.GetAxis("Mouse Y") * MoveSpeed
+			);
+	}
+}
