537e10203d875aef18ec37edf53d6b8b26f4f1f8
D. Max Loy <morac@morac.net>
2015-09-13 16:07:06 -0700

    Victory particles

--- Assets/Scripts/EndZone.cs
+++ Assets/Scripts/EndZone.cs
@@ -9,7 +9,15 @@ public class EndZone : MonoBehaviour
 		if(GameManager.CurrentState == GameManager.State.Playing && other.tag == "Player")
 		{
 			GameManager.Instance.GoToState(GameManager.State.Victory);
+
+			var body = other.GetComponent<Rigidbody>();
+			var particles = Instantiate(Resources.Load("VictoryParticles") as GameObject) as GameObject;
+			particles.transform.position = other.transform.position;
+			particles.transform.forward = Vector3.up;
+			particles.SetActive(true);
+
 			other.gameObject.SetActive(false);
+
 		}
 	}
 }
