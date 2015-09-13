b7caf0b3691d73f64d94b5a31efff722178518da
D. Max Loy <morac@morac.net>
2015-09-13 02:08:16 -0700

    cleanup and moving statement position

---
+++ Assets/Scripts/DampToOffset.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DampToOffset : MonoBehaviour {
+
+	public Transform Target;
+	public float DampAmount = 2f;
+
+	float offset;
+	Vector3 speed;
+
+	void Start()
+	{
+		offset = (Target.position - transform.position).magnitude;
+	}
+
+	void Update()
+	{
+		transform.position = Vector3.SmoothDamp(transform.position, Target.position + Target.forward * offset, ref speed, DampAmount);
+		transform.forward = Target.forward;
+		//transform.up = Vector3.up;
+		
+	}
+}
